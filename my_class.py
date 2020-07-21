ddb0e0c51fecfccc3be213e3b8de57c5e83a6480
Jowita Borowska <54434838+jowborowska@users.noreply.github.com>
2020-07-21 12:21:45 +0200

    Add files via upload

---
+++ my_class.py
@@ -0,0 +1,182 @@
+#Developing a class, which takes in a list of n maps (n map names, since we call map_cosmo inside this class)
+
+# Methods to write:----------------------------------------------------------------------------------------
+# one that computes all the cross spectra between the n feed-averaged maps
+# what is done in xs_feeds and xs_mean added as methods
+# method like calculate_feed_xs or something like that for the cross spectra of all combinaitons of feeds, so like xs_feeds (?)
+# maybe add a plotting method, such as the plotting from xs_script
+
+#Comments:-------------------------------------------------------------------------------------------------
+# we probably want to call the map_cosmo class from within the new cross spectrum class, since we do not neccesarily want to store all the maps in memory at once
+# when writing method calculate_feed_xs, change calling map_cosmo to have an option with specified feed or feed=None
+# maybe try to return the index of a given xs, so that we always know which maps were taken and can use it in plot
+# then download a third map and check if everything works fine
+# remember that these error bars come from random simulations, so they will be a bit different each time
+
+#Questions:-----------------------------------------------------------------------------------------------
+# Is "feed-averaged" map a one that has feed=None in map_cosmo ? (I think so)
+# Should I design make_h5 and run_noise_sims simulations work also for every combo of maps at once or take the pairs I want it for as an argument ? Or to have an option: all or given pairs
+#should I normalize weights differently depending on with which other map we calculate xs ? So basically call it inside i j for loop in calculate_xs - I think so
+
+import numpy as np
+import h5py
+import tools
+import map_cosmo
+import matplotlib.pyplot as plt
+
+class CrossSpectrum_nmaps():
+    def __init__(self, list_of_n_map_names):
+        self.maps = []
+        for map_name in list_of_n_map_names:
+           my_map = map_cosmo.MapCosmo(map_name) 
+           self.maps.append(my_map)
+        #self.weights_are_normalized = False
+   
+    #NORMALIZE WEIGHTS FOR A GIVEN PAIR OF MAPS
+    def normalize_weights(self, i, j):
+        norm = np.sqrt(np.mean((self.maps[i].w * self.maps[j].w).flatten()))
+        self.maps[i].w = self.maps[i].w / norm
+        self.maps[j].w = self.maps[j].w / norm
+       
+    #REVERSE NORMALIZE_WEIGHTS, TO NORMALIZE EACH PAIR OF MAPS INDEPENDENTLY
+    def reverse_normalization(self, i, j):    
+        norm = np.sqrt(np.mean((self.maps[i].w * self.maps[j].w).flatten()))
+        self.maps[i].w = self.maps[i].w*norm
+        self.maps[j].w = self.maps[j].w*norm
+
+    #INFORM WHICH XS INDEX CORRESPONDS TO WHICH MAP-PAIR
+    def get_information(self, list_of_n_map_names):
+        indexes_xs = []
+        index = -1 
+        for i in range(len(self.maps)):
+          for j in range(i,len(self.maps)): 
+             if i != j: 
+                index += 1
+                indexes_xs.append([index,list_of_n_map_names[i],list_of_n_map_names[j]])
+        return indexes_xs
+             
+    #COMPUTE ALL THE XS BETWEEN THE n FEED-AVERAGED MAPS
+    def calculate_xs(self, no_of_k_bins=15): #here take the number of k-bins as an argument 
+        n_k = no_of_k_bins
+        self.k_bin_edges = np.logspace(-2.0, np.log10(1.5), n_k)
+        
+        #store each cross-spectrum and corresponding k and nmodes by appending to these lists:
+        self.xs = []
+        self.k = []
+        self.nmodes = []
+        for i in range(len(self.maps)):
+           for j in range(i,len(self.maps)): #ensure that we don't compute the same pairs of maps twice <---------is this correct ?
+              if i != j: #ensure that we compute xs, not auto spectrum
+                 
+                 self.normalize_weights(i,j) #normalize weights for given xs pair of maps
+
+                 my_xs, my_k, my_nmodes = tools.compute_cross_spec3d(
+                 (self.maps[i].map * self.maps[i].w, self.maps[j].map * self.maps[j].w),
+                 self.k_bin_edges, dx=self.maps[i].dx, dy=self.maps[i].dy, dz=self.maps[i].dz)
+
+                 self.reverse_normalization(i,j) #go back to the previous state to normalize again with a different map-pair
+
+                 self.xs.append(my_xs)
+                 self.k.append(my_k)
+                 self.nmodes.append(my_nmodes)
+        self.xs = np.array(self.xs)
+        self.k = np.array(self.k)
+        self.nmodes = np.array(self.nmodes)
+        return self.xs, self.k, self.nmodes
+   
+    #RUN NOISE SIMULATIONS (for all combinations of n maps, to match xs)
+    def run_noise_sims(self, n_sims, seed=None):
+        self.rms_xs_mean = []
+        self.rms_xs_std = []
+        for i in range(len(self.maps)):
+           for j in range(i,len(self.maps)): #ensure that we don't take the same pairs of maps twice 
+              if i != j: #only for xs, not auto spectra
+
+                 self.normalize_weights(i,j)
+
+                 if seed is not None:
+                     if self.maps[i].feed is not None:
+                         feeds = np.array([self.maps[i].feed, self.maps[j].feed])
+                     else:
+                         feeds = np.array([1, 1])
+            
+                 rms_xs = np.zeros((len(self.k_bin_edges) - 1, n_sims))
+                 for g in range(n_sims):
+                     randmap = [np.zeros(self.maps[i].rms.shape), np.zeros(self.maps[i].rms.shape)]
+                     for l in range(len(self.maps)):
+                         if seed is not None:
+                             np.random.seed(seed * (g + 1) * (l + 1) * feeds[l])
+                         randmap[l] = np.random.randn(*self.maps[l].rms.shape) * self.maps[l].rms
+
+                     rms_xs[:, g] = tools.compute_cross_spec3d(
+                         (randmap[0] * self.maps[i].w, randmap[1] * self.maps[j].w),
+                         self.k_bin_edges, dx=self.maps[i].dx, dy=self.maps[i].dy, dz=self.maps[i].dz)[0]
+                 
+                 self.reverse_normalization(i,j) #go back to the previous state to normalize again with a different map-pair
+
+                 self.rms_xs_mean.append(np.mean(rms_xs, axis=1))
+                 self.rms_xs_std.append(np.std(rms_xs, axis=1))
+        return self.rms_xs_mean, self.rms_xs_std
+    
+    #MAKE SEPARATE H5 FILE FOR EACH XS
+    def make_h5(self, outname=None):
+        index = -1 
+        for i in range(len(self.maps)):
+           for j in range(i,len(self.maps)): #ensure that we don't take the same pairs of maps twice 
+              if i != j: #only for xs, not auto spectra
+                 index += 1 #find a correct xs, etc.
+
+                 if outname is None:
+                     tools.ensure_dir_exists('spectra')
+                     outname = 'spectra/xs' + self.maps[i].save_string + '_' + self.maps[j].map_string + '.h5'            
+
+                 f1 = h5py.File(outname, 'w')
+                 try:
+                     f1.create_dataset('mappath1', data=self.maps[i].mappath)
+                     f1.create_dataset('mappath2', data=self.maps[j].mappath)
+                     f1.create_dataset('xs', data=self.xs[index])
+                     f1.create_dataset('k', data=self.k[index])
+                     f1.create_dataset('k_bin_edges', data=self.k_bin_edges)
+                     f1.create_dataset('nmodes', data=self.nmodes[index])
+                 except:
+                     print('No power spectrum calculated.')
+                     return 
+                 try:
+                     f1.create_dataset('rms_xs_mean', data=self.rms_xs_mean[index])
+                     f1.create_dataset('rms_xs_std', data=self.rms_xs_std[index])
+                 except:
+                     pass
+                 
+                 f1.close()
+
+    #PLOT XS (previously in the script code)
+    def plot_xs(self, k, xs, rms_sig, rms_mean):
+       lim = np.mean(np.abs(xs[4:])) * 4
+       fig = plt.figure()
+
+       ax1 = fig.add_subplot(211)
+       ax1.errorbar(k, xs, rms_sig, fmt='o', label=r'$\tilde{C}_{data}(k)$')
+       ax1.plot(k, 0 * rms_mean, 'k', label=r'$\tilde{C}_{noise}(k)$', alpha=0.4)
+
+       ax1.set_ylabel(r'$\tilde{C}(k)$ [$\mu$K${}^2$ Mpc${}^3$]')
+       ax1.set_ylim(-lim, lim)              # ax1.set_ylim(0, 0.1)
+
+       ax1.set_xscale('log')
+       ax1.grid()
+       plt.legend()
+
+       ax2 = fig.add_subplot(212)
+       ax2.errorbar(k, xs / rms_sig, rms_sig / rms_sig, fmt='o', label=r'$\tilde{C}_{data}(k)$')
+       ax2.plot(k, 0 * rms_mean, 'k', alpha=0.4)
+       ax2.set_ylabel(r'$\tilde{C}(k) / \sigma_\tilde{C}$')
+       ax2.set_xlabel(r'$k$ [Mpc${}^{-1}$]')
+       ax2.set_ylim(-12, 12)
+       ax2.set_xscale('log')
+       ax2.grid()
+       plt.legend()
+
+       #plt.savefig('xs' + my_map.save_string + my_map2.save_string + '.png', bbox_inches='tight')
+
+       plt.show()
+
+
---
+++ my_script.py
@@ -0,0 +1,36 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import numpy.fft as fft
+import corner
+import h5py
+import sys
+
+import tools
+import map_cosmo
+import my_class
+
+n = len(sys.argv) - 1 #number of maps
+list_of_n_map_names = []
+
+for i in range(n):
+    list_of_n_map_names.append(sys.argv[i+1])
+if n < 2:
+    print('Missing filenames!')
+    print('Usage: python my_script.py mapname_1 mapname_2 ... mapname_n') #python my_script.py co6_013836_200601_map.h5 co6_013855_200602_map.h5
+    sys.exit(1)
+
+
+my_xs = my_class.CrossSpectrum_nmaps(list_of_n_map_names)
+
+calculated_xs = my_xs.get_information(list_of_n_map_names) 
+print calculated_xs #gives the xs, k, rms_sig, rms_mean index with corresponding map-pair
+
+xs, k, nmodes = my_xs.calculate_xs()
+
+rms_mean, rms_sig = my_xs.run_noise_sims(10) #these rms's are arrays of 14 elements, that give error bars (number of bin edges minus 1)
+
+my_xs.make_h5()
+
+my_xs.plot_xs(k[0], xs[0], rms_sig[0], rms_mean[0])
+
+
