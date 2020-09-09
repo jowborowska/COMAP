fda35aba5743b5258d04c63b3fdb5647045b5218
jowborowska <jowita.borowska@astro.uio.no>
2020-09-09 22:55:48 +0200

    paranthesis

--- map_cosmo.py
+++ map_cosmo.py
@@ -24,7 +24,7 @@ class MapCosmo():
             else: #create or read map_coadd - all the feeds 'added' together
                 
                 if jk == 'dayn' or jk == 'half':
-                   print 'Creating coadded feed map for the split.'
+                   print ('Creating coadded feed map for the split.')
                    self.map, self.rms = self.coadd_feed_maps(my_file,split_no,jk)
                 if jk == 'odde' or jk == 'sdlb':
                    self.map = np.array(my_file['/jackknives/map_' + jk][split_no]) 
