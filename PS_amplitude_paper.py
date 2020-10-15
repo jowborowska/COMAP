c43b9802265c2457ec3e1741e3cd9419b9ff18d3
Jowita Borowska <54434838+jowborowska@users.noreply.github.com>
2020-10-15 16:07:21 +0200

    Add files via upload

--- PS_amplitude_paper.py
+++ PS_amplitude_paper.py
@@ -176,18 +176,18 @@ def xs_with_model(figure_name, k, xs_mean_l, xs_mean_u, xs_sigma_l, xs_sigma_u):
    fig = plt.figure()
    ax1 = fig.add_subplot(211)
    ax1.errorbar(k, k * diff_mean / (transfer(k)*transfer_Nils(k)), k * error / (transfer(k)*transfer_Nils(k)), fmt='o', label=r'$k\tilde{C}_{diff}(k)$', color='teal')
-   
-   error_scaled = k * error / (transfer(k)*transfer_Nils(k))   
    '''
+   error_scaled = k * error / (transfer(k)*transfer_Nils(k))   
+   
    ax1.plot(k, k * diff_mean / (transfer(k)*transfer_Nils(k)),  label=r'$k\tilde{C}_{diff}(k)$', color='teal')
    ax1.fill_between(x=k, y1=k * diff_mean / (transfer(k)*transfer_Nils(k)) - error_scaled, y2=k * diff_mean / (transfer(k)*transfer_Nils(k)) + error_scaled, facecolor='paleturquoise', edgecolor='paleturquoise')
-   '''
+   
 
    ax1.plot(k, k * sum_mean / (transfer(k)*transfer_Nils(k)),  label=r'$k\tilde{C}_{sum}(k)$', color='purple')
    ax1.fill_between(x=k, y1=k * sum_mean / (transfer(k)*transfer_Nils(k)) - error_scaled, y2=k * sum_mean / (transfer(k)*transfer_Nils(k)) + error_scaled, facecolor='plum', edgecolor='plum')
+   '''
    
-   
-   #ax1.errorbar(k, k * sum_mean / (transfer(k)*transfer_Nils(k)), k * error / (transfer(k)*transfer_Nils(k)), fmt='o', label=r'$k\tilde{C}_{sum}(k)$', color='purple')
+   ax1.errorbar(k, k * sum_mean / (transfer(k)*transfer_Nils(k)), k * error / (transfer(k)*transfer_Nils(k)), fmt='o', label=r'$k\tilde{C}_{sum}(k)$', color='purple')
    #ax1.errorbar(k, k * xs_mean, k * xs_sigma, fmt='o', label=r'$k\tilde{C}_{data}(k)$')
    ax1.plot(k, 0 * xs_mean_l, 'k', alpha=0.4)
    #ax1.plot(k, k*PS_function.PS_f(k)/ transfer(k), label='k*PS of the input signal')
@@ -202,8 +202,8 @@ def xs_with_model(figure_name, k, xs_mean_l, xs_mean_u, xs_sigma_l, xs_sigma_u):
    plt.legend(bbox_to_anchor=(0, 0.61))
    ax1.legend()
    ax2 = fig.add_subplot(212)
-   ax2.errorbar(k, diff_mean / error, error /error, fmt='o', label=r'$\tilde{C}_{diff}(k)$', color='teal')
-   ax2.errorbar(k, sum_mean / error, error /error, fmt='o', label=r'$\tilde{C}_{sum}(k)$', color='purple')
+   ax2.errorbar(k, diff_mean / error, error /error, fmt='o', label=r'$\tilde{C}_{diff}(k)$', color='darkturquoise')
+   ax2.errorbar(k, sum_mean / error, error /error, fmt='o', label=r'$\tilde{C}_{sum}(k)$', color='plum')
    ax2.plot(k, 0 * xs_mean_l, 'k', alpha=0.4)
    ax2.set_ylabel(r'$\tilde{C}(k) / \sigma_\tilde{C}$')
    ax2.set_xlabel(r'$k$ [Mpc${}^{-1}$]')
