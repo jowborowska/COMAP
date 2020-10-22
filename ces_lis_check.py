d674d184c906c196f3da1e8dba999dfdc50cc013
Jowita Borowska <54434838+jowborowska@users.noreply.github.com>
2020-10-22 13:37:29 +0200

    Add files via upload

--- ces_lis_check.py
+++ ces_lis_check.py
@@ -1,15 +1,15 @@
 import h5py
 import numpy as np
 
-co7_ces = h5py.File('/mn/stornext/d16/cmbco/comap/protodir/maps/co6_map_complete_night_ces.h5', 'r')
-co7_liss = h5py.File('/mn/stornext/d16/cmbco/comap/protodir/maps/co6_map_complete_night_liss.h5', 'r')
-difference = h5py.File('co6_night_ces_liss_subtracted.h5', 'r')
+co7_ces = h5py.File('/mn/stornext/d16/cmbco/comap/protodir/maps/co7_map_complete_night_ces.h5', 'r')
+co7_liss = h5py.File('/mn/stornext/d16/cmbco/comap/protodir/maps/co7_map_complete_night_liss.h5', 'r')
+difference = h5py.File('co7_night_ces_liss_subtracted.h5', 'r')
 
 difference_map = difference['/jackknives/map_dayn'][0]
 
-ces = co7_ces['/jackknives/map_dayn'][0]
+ces = co7_ces['/jackknives/map_half'][0]
 
-liss = co7_liss['/jackknives/map_dayn'][0]
+liss = co7_liss['/jackknives/map_half'][0]
 
 print ('ces-lis: ')
 print (np.allclose(ces,liss))
@@ -17,4 +17,8 @@ print ('difference: ')
 print (np.max(np.abs(difference_map)))
 
 
+ces = co7_ces['/jackknives/map_sidr'][0]
 
+liss = co7_liss['/jackknives/map_sidr'][0]
+print ('ces-lis: ')
+print (np.allclose(ces,liss))
