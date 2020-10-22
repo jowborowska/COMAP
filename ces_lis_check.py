e4f723107720fccc896699b8274756980269611d
Jowita Borowska <54434838+jowborowska@users.noreply.github.com>
2020-10-22 13:33:19 +0200

    Add files via upload

--- ces_lis_check.py
+++ ces_lis_check.py
@@ -1,9 +1,9 @@
 import h5py
 import numpy as np
 
-co7_ces = h5py.File('/mn/stornext/d16/cmbco/comap/protodir/maps/co7_map_complete_night_ces.h5', 'r')
-co7_liss = h5py.File('/mn/stornext/d16/cmbco/comap/protodir/maps/co7_map_complete_night_liss.h5', 'r')
-difference = h5py.File('co7_night_ces_liss_subtracted.h5', 'r')
+co7_ces = h5py.File('/mn/stornext/d16/cmbco/comap/protodir/maps/co2_map_complete_night_ces.h5', 'r')
+co7_liss = h5py.File('/mn/stornext/d16/cmbco/comap/protodir/maps/co2_map_complete_night_liss.h5', 'r')
+difference = h5py.File('co2_night_ces_liss_subtracted.h5', 'r')
 
 difference_map = difference['/jackknives/map_dayn'][0]
 
@@ -14,5 +14,7 @@ liss = co7_liss['/jackknives/map_dayn'][0]
 print ('ces-lis: ')
 print (np.allclose(ces,liss))
 print ('difference: ')
-print (sum(difference_map))
 print (np.max(np.abs(difference_map)))
+
+
+
