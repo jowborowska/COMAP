d8ca2aed87cd0356b5cbb805ac77e5a7b3ac34e7
Jowita Borowska <54434838+jowborowska@users.noreply.github.com>
2020-10-26 15:57:41 +0100

    Add files via upload

--- check_new_map.py
+++ check_new_map.py
@@ -4,13 +4,13 @@ import h5py
 f = h5py.File('/mn/stornext/d16/cmbco/comap/protodir/maps/co7_map_complete_sunel.h5', 'r')
 keys_list = list(f.keys())
 
-print keys_list
+print (keys_list)
 '''
 [u'feeds', u'freq', u'jackknives', u'map', u'map_coadd', u'mean_az', u'mean_el', u'n_x', u'n_y', u'nhit', u'nhit_coadd', u'njk', u'nside', u'nsim', u'patch_center', u'rms', u'rms_coadd', u'time', u'x', u'y']
 '''
 
 jackknives = f['jackknives']
-print jackknives.keys()
+print (jackknives.keys())
 '''
 [u'jk_def', u'jk_feedmap', u'map_dayn', u'map_half', u'map_odde', u'map_sdlb', u'nhit_dayn', u'nhit_half', u'nhit_odde', u'nhit_sdlb', u'rms_dayn', u'rms_half', u'rms_odde', u'rms_sdlb']
 '''
