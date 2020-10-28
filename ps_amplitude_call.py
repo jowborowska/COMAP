0cbb156f6289f9c1c714d999dfadf1c5b4199822
Jowita Borowska <54434838+jowborowska@users.noreply.github.com>
2020-10-28 14:56:20 +0100

    Add files via upload

--- ps_amplitude_call.py
+++ ps_amplitude_call.py
@@ -347,13 +347,13 @@ xs_with_model('xs_mean_sim_null.png', k_sim1, xs_mean_sim1, xs_mean_sim2, xs_sig
 
 def call_all(mapname, split):
    xs_files = 'spectra/xs_' + mapname + '_1st_' + split + '_feed%01i_and_' + mapname +'_2nd_' + split +'_feed%01i.h5'
-   kl, xs_mean_l, xs_sigma_l = xs_feed_feed_grid_new(xs_files, 'xs_grid' +mapname + '_lower.png', ' of 1st ' + split + ' split', ' of 2nd ' + split + ' split', True)
-   ku, xs_mean_u, xs_sigma_u = xs_feed_feed_grid_new(xs_files, 'xs_grid' +mapname + '_upper.png', ' of 1st ' + split + ' split', ' of 2nd ' + split + ' split', False)
+   kl, xs_mean_l, xs_sigma_l = xs_feed_feed_grid_upper_half(xs_files, 'xs_grid' +mapname + '_lower.png', ' of 1st ' + split + ' split', ' of 2nd ' + split + ' split')
+   ku, xs_mean_u, xs_sigma_u = xs_feed_feed_grid_lower_half(xs_files, 'xs_grid' +mapname + '_upper.png', ' of 1st ' + split + ' split', ' of 2nd ' + split + ' split', False)
    xs_with_model('xs_mean' + mapname + '_null.png', kl, xs_mean_l, xs_mean_u, xs_sigma_l, xs_sigma_u, mapname + ', '+ split + 'split')
    print ("Created files:")
-   print('xs_grid' +mapname + '_lower.png')
-   print('xs_grid' +mapname + '_upper.png')
-   print('xs_mean' + mapname + '_null.png')
+   print('xs_grid_' +mapname + '_lower.png')
+   print('xs_grid_' +mapname + '_upper.png')
+   print('xs_mean_' + mapname + '_null.png')
 
 call_all('co2_map_complete_sunel_ces', 'dayn')
 '''
