8c960d5446505da22fe09bfed13c39e4e27eae0c
Jowita Borowska <54434838+jowborowska@users.noreply.github.com>
2020-11-03 13:02:10 +0100

    Add files via upload

--- proposal.py
+++ proposal.py
@@ -98,13 +98,17 @@ def xs_feed_feed_grid(path_to_xs, figure_name, split1, split2):
 
 
 
-def xs_with_model_3fields(figure_name, k, xs_mean2, xs_mean6, xs_mean7, xs_sigma2, xs_sigma6, xs_sigma7, titlename):
+def xs_with_model_3fields(figure_name, k, xs_mean2, xs_mean6, xs_mean7, xs_sigma2, xs_sigma6, xs_sigma7, scan_strategy):
   
+   if scan_strategy == 'ces':
+      titlename = 'CES scans, night time data'
+   if scan_strategy == 'liss':
+      titlename = 'Lissajous scans, night time data'
    transfer = scipy.interpolate.interp1d(k_th, ps_th / ps_th_nobeam) #transfer(k) always < 1, values at high k are even larger and std as well
    transfer_Nils = scipy.interpolate.interp1d(k_Nils, T_Nils) 
    P_theory = scipy.interpolate.interp1d(k_th,ps_th_nobeam)
    
-   k_offset = k*0.05
+   k_offset = k*0.02
    k6 = k - k_offset
    k7 = k + k_offset
    lim = np.mean(np.abs(xs_mean2[4:-2] * k[4:-2])) * 8
@@ -122,7 +126,10 @@ def xs_with_model_3fields(figure_name, k, xs_mean2, xs_mean6, xs_mean7, xs_sigma
    #ax1.plot(k_th, k_th * ps_th_nobeam * 10, '--', label=r'$10 \times kP_{Theory}(k)$', color='dodgerblue')
    #ax1.plot(k_th, k_th * ps_copps_nobeam * 5, 'g--', label=r'$5 \times kP_{COPPS}$ (shot)')
    ax1.set_ylabel(r'$k\tilde{C}(k)$ [$\mu$K${}^2$ Mpc${}^2$]', fontsize=14)
-   ax1.set_ylim(-lim*3, lim*3)              # ax1.set_ylim(0, 0.1)
+   if scan_strategy == 'ces':
+      ax1.set_ylim(-lim*3, lim*3)              # ax1.set_ylim(0, 0.1)
+   if scan_strategy == 'liss':
+      ax1.set_ylim(-lim, lim)              # ax1.set_ylim(0, 0.1)
    ax1.set_xlim(0.04,1)
    ax1.set_xscale('log')
    ax1.set_title(titlename)
@@ -169,12 +176,12 @@ def call_all(mapname, split):
 k2c, mean2c, sigma2c = call_all('co2_map_complete_sunel_ces', 'dayn')
 k6c, mean6c, sigma6c = call_all('co6_map_complete_sunel_ces', 'dayn')
 k7c, mean7c, sigma7c = call_all('co7_map_complete_sunel_ces', 'dayn')
-xs_with_model_3fields('xs_mean_' + 'ces' + '_' + 'dayn' + '.pdf', k2c, mean2c, mean6c, mean7c, sigma2c, sigma6c, sigma7c, 'CES scans, night time data')
+xs_with_model_3fields('xs_mean_' + 'ces' + '_' + 'dayn' + '.pdf', k2c, mean2c, mean6c, mean7c, sigma2c, sigma6c, sigma7c, 'ces')
 print ('xs_mean_' + 'ces' + '_' + 'dayn' + '.pdf')
 
 k2l, mean2l, sigma2l = call_all('co2_map_complete_sunel_liss', 'dayn')
 k6l, mean6l, sigma6l = call_all('co6_map_complete_sunel_liss', 'dayn')
 k7l, mean7l, sigma7l = call_all('co7_map_complete_sunel_liss', 'dayn')
-xs_with_model_3fields('xs_mean_' + 'liss' + '_' + 'dayn' + '.pdf', k2l, mean2l, mean6l, mean7l, sigma2l, sigma6l, sigma7l, 'Lissajous scans, night time data')
+xs_with_model_3fields('xs_mean_' + 'liss' + '_' + 'dayn' + '.pdf', k2l, mean2l, mean6l, mean7l, sigma2l, sigma6l, sigma7l, 'liss')
 print ('xs_mean_' + 'liss' + '_' + 'dayn' + '.pdf')
 
